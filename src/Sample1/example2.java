8825a266f06e9d097d566013a3e6d96ea725efe8
Rahul <Rahul@DESKTOP-RDTSFOS>
2022-09-15 18:25:16 +0530

    [rahul]-program changed

--- src/Sample1/example2.java
+++ src/Sample1/example2.java
@@ -1,10 +1,13 @@
 package Sample1;
 
-public class example2 {
+public class example2
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		System.out.println("gud eve");
+		System.out.println("good evening");
 	}
 
 }
